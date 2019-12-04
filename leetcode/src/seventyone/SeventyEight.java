57f09a2ae4a6b80407a33cefce776a7cb9aa8ad6
skpupil <1452099719@qq.com>
2019-12-04 19:39:24 +0800

    222

--- leetcode/src/seventyone/SeventyEight.java
+++ leetcode/src/seventyone/SeventyEight.java
@@ -1,7 +1,26 @@
 package seventyone;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SeventyEight {
 
+	public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        sol(0,ans,new ArrayList<>(),nums);
+        return ans;
+    }
+	
+	public void sol(int indexx,List<List<Integer>> ans,List<Integer> tmp,int[] nums) {
+		ans.add(new ArrayList<>(tmp));
+		for(int i = indexx;i<nums.length;i++) {
+			tmp.add(nums[i]);
+			sol(i+1,ans,tmp,nums);
+			tmp.remove(tmp.size()-1);
+			//sol(indexx+1,ans,tmp,nums);
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
